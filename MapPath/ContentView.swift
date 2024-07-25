b6ca7df08526e83296d51eb1eab98d494212a07a
Hendra Lijaya <hendralijaya@gmail.com>
2024-07-25 14:04:01 +0700

    Initial Commit

---
+++ MapPath/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapPath
+//
+//  Created by hendra on 25/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapPath/MapPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapPathApp.swift
+//  MapPath
+//
+//  Created by hendra on 25/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MapPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
